f12157fe26cfebcf41ae82924ec46f7ec1417a42
Kirill L <lobachevsky.kerya@gmail.com>
2025-07-23 22:11:30 +0300

    added hero screen image animation

--- src/components/heroScreen/hero-screen-components/HeroScreenTextItem.tsx
+++ src/components/heroScreen/hero-screen-components/HeroScreenTextItem.tsx
@@ -4,7 +4,7 @@ export function HeroScreenTextItem({text}: {text: string}) {
 	const typed = useTypeWriter(text)
 
 	return (
-		<div className='whitespace-pre-line text-2xl max-md:text-xl'>
+		<div className='whitespace-pre-line text-2xl max-lg:text-lg'>
 			<span>&gt; </span>
 			<span>{typed}</span>
 		</div>
