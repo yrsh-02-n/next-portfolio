fe9acaa06834bac190e6466de7a37f5c68e901c2
Kirill L <lobachevsky.kerya@gmail.com>
2025-08-05 19:28:05 +0300

    fix build: header.data.ts error

--- src/components/layout/header/header.data.ts
+++ src/components/layout/header/header.data.ts
@@ -7,20 +7,17 @@ export const HEADER_DATA: IMenuItem[] = [
 	{
 		label: 'я',
 		link: PAGE.ABOUT,
-		isActive: false,
 		title: 'О моем опыте'
 	},
 	{
 		label: 'frontend-разработчик',
 		link: PAGE.PORTFOLIO_DEV,
-		isActive: false,
 		title: 'Портфолио: frontend-разработка',
 		icon: Code
 	},
 	{
 		label: 'web-дизайнер',
 		link: PAGE.PORTFOLIO_DESIGN,
-		isActive: false,
 		title: 'Портфолио: web-дизайн',
 		icon: PanelsTopLeft
 	}
