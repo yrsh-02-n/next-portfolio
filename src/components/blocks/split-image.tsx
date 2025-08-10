5252343388bf80ae4826db4f3e008e6e7a3856b7
Kirill L <lobachevsky.kerya@gmail.com>
2025-08-10 23:41:59 +0300

    fix: sanity blocks after deploy

--- src/components/blocks/split-image.tsx
+++ src/components/blocks/split-image.tsx
@@ -1,8 +1,8 @@
 import { PortableText, stegaClean } from 'next-sanity'
 import Image from 'next/image'
 import { PORTFOLIO_CASE_PAGE_BY_CATEGORYResult } from 'sanity.types'
-import { urlFor } from '@/sanity/lib/image'
 
+import { urlFor } from '@/sanity/lib/image'
 
 type SplitImageProps = Extract<
 	NonNullable<NonNullable<PORTFOLIO_CASE_PAGE_BY_CATEGORYResult>['content']>[number],
@@ -12,10 +12,10 @@ type SplitImageProps = Extract<
 export function SplitImage({ text, image, orientation }: SplitImageProps) {
 	return (
 		<div
-			className="flex items-top gap-[2rem] data-[orientation='imageRight']:flex-row-reverse text-balance mb-[4rem]"
+			className="flex items-top gap-[2rem] data-[orientation='imageRight']:flex-row-reverse text-balance mb-[4rem] max-md:data-[orientation='imageRight']:flex-col max-md:gap-[3rem]"
 			data-orientation={stegaClean(orientation) || 'imageLeft'}
 		>
-			<div className='relative aspect-[16/9] h-full w-[50%] max-md:hidden'>
+			<div className='relative aspect-[16/9] h-full w-[50%] max-md:order-2 max-md:w-full'>
 				{image?.asset?.url ? (
 					<Image
 						src={urlFor(image).url()}
@@ -25,7 +25,7 @@ export function SplitImage({ text, image, orientation }: SplitImageProps) {
 					/>
 				) : null}
 			</div>
-			<div className='w-[50%] max-md:w-full text-2xl max-xl:text-xl max-md:text-lg text-primary flex flex-col gap-[1rem] sanity-block'>
+			<div className='w-[50%] max-md:w-full text-2xl max-xl:text-xl max-md:text-lg text-primary flex flex-col gap-[1rem] sanity-block max-md:order-1'>
 				{text ? <PortableText value={text} /> : null}
 			</div>
 		</div>
